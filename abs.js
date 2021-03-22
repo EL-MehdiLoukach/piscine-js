635671e7b0a7dbb30cb1b9203eaf7560de374fb2
pierre <pierre.gourgouillon@ynov.com>
2021-03-22 13:08:07 +0100

    abs

---
+++ abs.js
@@ -0,0 +1,20 @@
+function isPositive(number){
+    if (number >= 0){
+        return true
+    }else{
+        return false
+    }
+}
+
+function abs(number){
+    if (number >= 0){
+        return number
+    }else{
+        let compteur = 0
+        for(let i = 0;i >= number;i--){
+            compteur += 1
+        }
+
+        return compteur
+    }
+}
