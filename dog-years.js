e5bb38d60dcb3d761138c35ebf4f25f9d50f9961
pierre <pierre.gourgouillon@ynov.com>
2021-03-22 16:00:19 +0100

    dog

--- dog-years.js
+++ dog-years.js
@@ -5,55 +5,46 @@ function dogYears(planet,yearSeconds){
         let b = parseFloat(a)
         return b
     }else if (planet == "mercury"){
-        let second = 31557600 / 0.2408467
-        let multiplicateur = 0.2408467 * 7
-        let result = yearSeconds / second * multiplicateur
+        let secondOnYear = 0.2408467 * 31557600
+        let result = yearSeconds / secondOnYear * 7
         let a = result.toFixed(2)
         let b = parseFloat(a)
-
         return b
-
-
-
-
-
-
-
-
-
-
-
-
-
     }else if (planet == "venus "){
-        let secondInThisPlanet = 0.61519726 * 31557600
-        let multiply =0.61519726 * 7 
-        let result = Math.floor(yearSeconds)/secondInThisPlanet* multiply
-        return result
+        let secondOnYear = 0.61519726 * 31557600
+        let result = yearSeconds / secondOnYear * 7
+        let a = result.toFixed(2)
+        let b = parseFloat(a)
+        return b
     }else if (planet == "mars"){
-        let secondInThisPlanet = 1.8808158 * 31557600
-        let multiply = 1.8808158 * 7 
-        let result = Math.floor(yearSeconds)/secondInThisPlanet* multiply
-        return result
+        let secondOnYear = 1.8808158 * 31557600
+        let result = yearSeconds / secondOnYear * 7
+        let a = result.toFixed(2)
+        let b = parseFloat(a)
+        return b
     }else if (planet == "jupiter"){
-        let secondInThisPlanet = 11.862615 * 31557600
-        let multiply = 11.862615 * 7 
-        let result = Math.floor(yearSeconds)/secondInThisPlanet* multiply
-        return result
+        let secondOnYear = 11.862615 * 31557600
+        let result = yearSeconds / secondOnYear * 7
+        let a = result.toFixed(2)
+        let b = parseFloat(a)
+        return b
     }else if (planet == "saturn"){
-        let secondInThisPlanet = 29.447498 * 31557600
-        let multiply = 29.447498 * 7 
-        let result = Math.floor(yearSeconds)/secondInThisPlanet* multiply
-        return result
+        let secondOnYear = 29.447498 * 31557600
+        let result = yearSeconds / secondOnYear * 7
+        let a = result.toFixed(2)
+        let b = parseFloat(a)
+        return b
     }else if (planet == "uranus"){
-        let secondInThisPlanet = 84.016846 * 31557600
-        let multiply = 84.016846 * 7 
-        let result = Math.floor(yearSeconds)/secondInThisPlanet* multiply
-        return result
+        let secondOnYear =  84.016846 * 31557600
+        let result = yearSeconds / secondOnYear * 7
+        let a = result.toFixed(2)
+        let b = parseFloat(a)
+        return b
     }else if (planet == "neptune"){
-        let secondInThisPlanet = 164.79132 * 31557600
-        let multiply = 164.79132 * 7 
-        let result = Math.floor(yearSeconds)/secondInThisPlanet* multiply
-        return result
+        let secondOnYear =  164.79132 * 31557600
+        let result = yearSeconds / secondOnYear * 7
+        let a = result.toFixed(2)
+        let b = parseFloat(a)
+        return b
     }
 }
