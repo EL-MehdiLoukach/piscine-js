d1a8d24b2ccabffcf9a9ce3a181125ae4ecba75d
pierre <pierre.gourgouillon@ynov.com>
2021-03-22 18:25:35 +0100

    reverser

--- reverser.js
+++ reverser.js
@@ -1,11 +1,23 @@
 function reverse(array){
-    let arrayTotal =[]
-    for(let i=array.length-1;i>=0;i--){
-        arrayTotal.push(array[i])
-    }
 
-    return arrayTotal
+    if (typeof array=="object"){
+        let arrayTotal =[]
+        for(let i=array.length-1;i>=0;i--){
+            arrayTotal.push(array[i])
+        }
+    
+        return arrayTotal
+    }else if(typeof array=="string"){
+
+        let arrayTotal =""
+        for(let i=array.length-1;i>=0;i--){
+            arrayTotal += array[i]
+        }
+    
+        return arrayTotal
+    }
+  
 }
 
 
-console.log(reverse(["5",4,87,"wesh"]))
+console.log(reverse("je"))
