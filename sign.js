85d5490930d3a3f1c3310248ab1af714c95fe65c
pierre <pierre.gourgouillon@ynov.com>
2021-03-22 13:44:48 +0100

    sign

---
+++ sign.js
@@ -0,0 +1,19 @@
+function sign( argument){
+    if (argument == 0){
+        return 0
+    }else if(argument > 0){
+        return 1
+    }else if (argument < 0){
+        return -1
+    }
+}
+
+function sameSign(argument1,argument2){
+    if (argument1 >= 0 && argument2 >= 0){
+        return true
+    }else if (argument1 < 0 && argument2 < 0){
+        return true
+    }
+
+    return false
+}
