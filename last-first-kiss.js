cf6f09147e38b5492a52255e02fc0ed2e0d99a60
pierre <pierre.gourgouillon@ynov.com>
2021-03-22 11:37:38 +0100

    last-first

---
+++ last-first-kiss.js
@@ -0,0 +1,12 @@
+function first(argument){
+    return argument[0]
+}
+
+function last(argument){
+    return argument[argument.length - 1]
+}
+
+function kiss(argument){
+    const table = [argument[argument.length - 1],argument[0]]
+    return table
+}
