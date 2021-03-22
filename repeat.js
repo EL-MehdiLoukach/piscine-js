1fcc251875f066355037dbdf3cd934d4dc4a3bbb
pierre <pierre.gourgouillon@ynov.com>
2021-03-22 18:09:08 +0100

    repeat

--- repeat.js
+++ repeat.js
@@ -1,7 +1,13 @@
 function repeat(words,number){
     let stockWords = ""
+    let numbers = parseInt(number)
+    if (number==0){
+        return ""
+    }else if (number<0){
+        return RangeError
+    }
 
-    for (let i=0;i<=number;i++){
+    for (let i=0;i<=numbers;i++){
         stockWords += words
     }
 
