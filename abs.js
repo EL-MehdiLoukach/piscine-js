ccedb8cffa8f2721896f6d5cb2e4ced7cbce7e80
pierre <pierre.gourgouillon@ynov.com>
2021-03-22 13:13:28 +0100

    abs

--- abs.js
+++ abs.js
@@ -9,6 +9,8 @@ function isPositive(number){
 function abs(number){
     if (number > 0){
         return number
+    }else if (number ==0){
+        return 0
     }else{
         let compteur = 0
         for(let i = 0;i >= number;i--){
