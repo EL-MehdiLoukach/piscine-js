24439a38feac974a3a602082c26d1d60d20757c0
pierre <pierre.gourgouillon@ynov.com>
2021-03-22 14:45:57 +0100

    dog

---
+++ dog-years.js
@@ -0,0 +1,42 @@
+function dogYears(planet,yearSeconds){
+    if (planet == "earth"){
+        let secondToMinute = Math.floor(yearSeconds)/31557600*7
+        secondToMinute = Math.floor(secondToMinute)
+        return secondToMinute
+    }else if (planet == "mercury"){
+        let secondInThisPlanet = 0.2408467 * 31557600
+        let multiply = 0.2408467 * 7 
+        let result = Math.floor(yearSeconds)/secondInThisPlanet* multiply
+        return result
+    }else if (planet == "venus "){
+        let secondInThisPlanet = 0.61519726 * 31557600
+        let multiply =0.61519726 * 7 
+        let result = Math.floor(yearSeconds)/secondInThisPlanet* multiply
+        return result
+    }else if (planet == "mars"){
+        let secondInThisPlanet = 1.8808158 * 31557600
+        let multiply = 1.8808158 * 7 
+        let result = Math.floor(yearSeconds)/secondInThisPlanet* multiply
+        return result
+    }else if (planet == "jupiter"){
+        let secondInThisPlanet = 11.862615 * 31557600
+        let multiply = 11.862615 * 7 
+        let result = Math.floor(yearSeconds)/secondInThisPlanet* multiply
+        return result
+    }else if (planet == "saturn"){
+        let secondInThisPlanet = 29.447498 * 31557600
+        let multiply = 29.447498 * 7 
+        let result = Math.floor(yearSeconds)/secondInThisPlanet* multiply
+        return result
+    }else if (planet == "uranus"){
+        let secondInThisPlanet = 84.016846 * 31557600
+        let multiply = 84.016846 * 7 
+        let result = Math.floor(yearSeconds)/secondInThisPlanet* multiply
+        return result
+    }else if (planet == "neptune"){
+        let secondInThisPlanet = 164.79132 * 31557600
+        let multiply = 164.79132 * 7 
+        let result = Math.floor(yearSeconds)/secondInThisPlanet* multiply
+        return result
+    }
+}
