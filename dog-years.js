622a2feef1005a8ccc3b074002b7b630c53ca7cb
pierre <pierre.gourgouillon@ynov.com>
2021-03-22 16:01:40 +0100

    dog

--- dog-years.js
+++ dog-years.js
@@ -10,7 +10,7 @@ function dogYears(planet,yearSeconds){
         let a = result.toFixed(2)
         let b = parseFloat(a)
         return b
-    }else if (planet == "venus "){
+    }else if (planet == "venus"){
         let secondOnYear = 0.61519726 * 31557600
         let result = yearSeconds / secondOnYear * 7
         let a = result.toFixed(2)
