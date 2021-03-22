e863f557a0e4312a010e5104e358cb5380f550dc
pierre <pierre.gourgouillon@ynov.com>
2021-03-22 12:38:49 +0100

    method-man

---
+++ method-man.js
@@ -0,0 +1,34 @@
+function word(argument){
+    const a = argument.split(" ")
+    return a
+}
+
+function sentence(table){
+    let word = ""
+    for (let i = 0;i < table.length; i++){
+        word = table[i] + " "
+    }
+    return word
+}
+
+function yell(word){
+    return word.toUpperCase()
+}
+
+function whisper(word){
+    let arg = "*"
+    arg += word + "*"
+    return arg
+}
+
+function Capitalize(word){
+    let maj =word[0]
+    maj = maj.toUpperCase()
+
+    number = word.length()-1
+    
+    min = str.substr(1,number)
+    min = min.toLowerCase()
+    let words = maj + min
+    return words
+}
