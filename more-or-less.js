dd3c082b036c80f34b02b0a42a2aa159352c34db
pierre <pierre.gourgouillon@ynov.com>
2021-03-22 11:23:28 +0100

    more-less

---
+++ more-or-less.js
@@ -0,0 +1,15 @@
+function more(number){
+    number + 1
+}
+
+function less(number){
+    number - 1
+}
+
+function add(number,number1){
+    number + number1
+}
+
+function sub(number,number1){
+    number - number1
+}
