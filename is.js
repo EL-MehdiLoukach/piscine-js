e96f8d786c76ee38361555a57b1fd71ae944a7ee
pierre <pierre.gourgouillon@ynov.com>
2021-03-22 16:54:51 +0100

    is

--- is.js
+++ is.js
@@ -1,17 +1,37 @@
 
 is.num = function(number){
-    return typeof number
+    return typeof number==Number
 }
 
 is.nan = function(number){
-    return typeof number
+    return typeof number==NaN
+}
+
+is.str = function(number){
+    return typeof number==String
+}
+
+is.bool = function(number){
+    return typeof number==Boolean
 }
 
 
-is.str= function(number){
+
+is.undef= function(number){
+    return typeof number==undefined
+}
+
+is.def = function(number){
     return typeof number
 }
 
-is.bool= function(number){
-    return typeof number
+is.arr = function(number){
+    return typeof number==Array
 }
+
+is.obj = function(number){
+    return typeof number == Object
+}
+
+
+
