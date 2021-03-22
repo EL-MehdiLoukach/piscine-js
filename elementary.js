35c6b16abc9389464a87c967c4c4d3cd5d2a7506
pierre <pierre.gourgouillon@ynov.com>
2021-03-22 21:30:57 +0100

    elementary

--- elementary.js
+++ elementary.js
@@ -1,8 +1,31 @@
 function multiply(number,number1){
     let calcul = 0
-    for(let i=0;i<number1;i++){
-        calcul += number
+    let NUMBER = number
+    let NUMBER1 = number1
+
+    if (number <0 ){
+        let a = number.toString()
+        let c = a.slice(1,number.length)
+        NUMBER = parseFloat(c)
     }
+
+    if (number1 < 0){
+        let a = number1.toString()
+        let c = a.slice(1,number1.length)
+        NUMBER1 = parseFloat(c)
+    }
+
+    for(let i=0;i<NUMBER1;i++){
+        calcul += NUMBER
+    }
+
+    if (number< 0 ||number1 < 0){
+        let c = "-"+calcul.toString()
+        let resultFinal = parseFloat(c)
+
+        return resultFinal
+    }
+    
     return calcul
 }
 
@@ -19,6 +42,6 @@ function modulo(number,number1){
     let calcul =number - (multiply((Math.round(number/number1)),number1))
     return calcul
 }   
-console.log(divide(15,3))
-console.log(modulo(9,4))
+console.log(multiply(4,4))
+//console.log(modulo(9,4))
 
