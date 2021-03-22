120a06d46abad09748c1b7015f7a24ecc0686ba1
pierre <pierre.gourgouillon@ynov.com>
2021-03-22 11:06:08 +0100

    declarations

--- declarations.js
+++ declarations.js
@@ -1,13 +1,13 @@
 const escapeStr = "\`\\/\"\'"
 const arr = [4,'2']
-Object.freeze(arr)
+
 const obj = {
     str : "hello",
     num : 15,
     bool : true,
     undef : undefined,
 }
-Object.freeze(obj)
+
 
 const nested = {
     arr : [4,undefined,'2'],
@@ -18,5 +18,8 @@ const nested = {
     }
 }
 Object.freeze(arr, obj)
-Object.freeze(nested,nested.arr,nested.obj)
+Object.freeze(obj)
+Object.freeze(nested)
+Object.freeze(nested.arr)
+Object.freeze(nested.obj)
 
