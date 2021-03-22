dfd170273a70183d7f5e813704306e34df8a0686
pierre <pierre.gourgouillon@ynov.com>
2021-03-22 23:28:11 +0100

    elementary

--- elementary.js
+++ elementary.js
@@ -52,19 +52,22 @@ function divide(resultat,multiplicateur){
 
     for (let i=RESULTAT-1;i>=1;i--){
         if (multiply(i,MULTIPLICATEUR) <= RESULTAT){
-            console.log("i:",i)
+            //console.log("i:",i)
             resultatDivision += i
-            console.log("division:",resultatDivision)
+            //console.log("division:",resultatDivision)
             RESULTAT -= multiply(i,MULTIPLICATEUR)
-            console.log("resultat:",RESULTAT)
+            //console.log("resultat:",RESULTAT)
         }
+    }
+
+    if (resultat < 0  && multiplicateur < 0){
+        return resultatDivision
     }
 
     if (resultat < 0 || multiplicateur <0){
         resultatDivision = -resultatDivision
         return resultatDivision
     }
-    
 
     return resultatDivision
 }
@@ -73,5 +76,5 @@ function modulo(number,number1){
     let calcul =number - (multiply((Math.round(divide(number,number1))),number1))
     return calcul
 }   
-console.log(divide(235,5))
+console.log(divide(123,-22))
 
