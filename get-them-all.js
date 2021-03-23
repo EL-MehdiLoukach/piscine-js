c1e5b8208ea4c9310d4990d6d72104e8f8397757
pierre <pierre.gourgouillon@ynov.com>
2021-03-23 21:26:17 +0100

    get-them-all

---
+++ get-them-all.js
@@ -0,0 +1,25 @@
+
+
+export function getArchitects(){
+let architectArray = []
+let noArchitectArray =[]
+let tableFinal = []
+architectArray = document.getElementsByTagName("a")
+noArchitectArray = document.getElementsByTagName("span")
+
+tableFinal.push(architectArray,noArchitectArray)
+console.log(tableFinal)
+return tableFinal
+}
+
+export function getClassical(){
+
+}
+
+export function getActive(){
+    
+}
+
+export function getBonannoPisano(){
+    
+}
