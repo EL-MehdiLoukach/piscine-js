5b7767af23be9202c0feace08069a6e91a71c64b
pierre <pierre.gourgouillon@ynov.com>
2021-03-23 18:42:53 +0100

    chunky

--- chunky.js
+++ chunky.js
@@ -1,14 +1,11 @@
 function chunk(array,size){
 
     let tableFinal = []
-
-    if(array.length%size == 0){
-        for(let i=0;i<array.length;i+=size){
-            tableFinal.push(array.slice(i,i+size))
-        }
-    }else{
-        tableFinal = array.slice(-size)
+    
+    for(let i=0;i<array.length;i+=size){
+        tableFinal.push(array.slice(i,i+size))
     }
+    
 
     return tableFinal
 }
