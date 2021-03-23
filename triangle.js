083fa145f7d33e9d7cfc6b6ce7af7ce6e7621a87
pierre <pierre.gourgouillon@ynov.com>
2021-03-23 14:53:48 +0100

    triangle

--- triangle.js
+++ triangle.js
@@ -1,18 +1,20 @@
 var stringFinal = ""
 
 function triangle(character,number){
-
     if (number == 1){
         stringFinal +=character
         stringFinal +="\n"
         return stringFinal
     }
     triangle(character,number-1)
+
     for(let i=1;i <=number;i++){
         stringFinal += character
     }
+
     stringFinal +="\n"
-    
-return stringFinal
+
+    return stringFinal
+
 }
 console.log(triangle("*",4))
