a6201718d1575a72d1e57b5dd8935144ace721b2
pierre <pierre.gourgouillon@ynov.com>
2021-03-23 21:45:52 +0100

    get-them-all

--- get-them-all.js
+++ get-them-all.js
@@ -4,11 +4,23 @@ export function getArchitects(){
 let architectArray = []
 let noArchitectArray =[]
 let tableFinal = []
-architectArray = document.getElementsByTagName("a")
-noArchitectArray = document.getElementsByTagName("span")
+let linkArray = document.getElementsByTagName("a")
+let spanArray= document.getElementsByTagName('span')
+
+for(let i = 0;i<linkArray.length;i++){
+    let idLink = linkArray[i].getAttribute('id')
+    architectArray.push(idLink)
+}
+
+for(let i=0;i<spanArray.length;i++){
+    let idSpan = spanArray[i].getAttribute('id')
+    noArchitectArray.push(idSpan)
+}
 
 tableFinal.push(architectArray,noArchitectArray)
+
 console.log(tableFinal)
+
 return tableFinal
 }
 
