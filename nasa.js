e1e9ae36eb31f31d390dff755d62c079d89eaffc
pierre <pierre.gourgouillon@ynov.com>
2021-03-23 13:06:47 +0100

    nasa

--- nasa.js
+++ nasa.js
@@ -2,25 +2,27 @@ function nasa(number){
     let stringFinal =""
     for(let i= 1;i<=number;i++){
 
-        if (i==1){
-            stringFinal += i.toString() + " "
-            continue
-        }
-
         if(i%3==0 && i%5==0){
             if (i==number){
                 stringFinal += "NASA"
-                continue
             }else{
                 stringFinal += "NASA "
             }
-        }else if(i%3==0){
-            stringFinal += "NA "
-        }else if(i%5==0){
-            stringFinal += "SA "
-        }else{
-            stringFinal += i.toString() + " "
+            continue
         }
+        
+        if(i%3==0){
+            stringFinal += "NA "
+            continue
+        }
+
+        if (i%5==0){
+            stringFinal += "SA "
+            continue
+        }
+        
+        stringFinal += i.toString() + " "
+        
     }
     return stringFinal
 }
