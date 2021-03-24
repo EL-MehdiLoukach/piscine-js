134f9e7864b29f1c84c40cba0727c699f2ee1579
pierre <pierre.gourgouillon@ynov.com>
2021-03-24 17:26:15 +0100

    keycodes-symphony

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -1,7 +1,16 @@
 export function compose(){
+
     let body = document.querySelector("body")
+    let alphabet = "abcdefghijklmnopqrstuvwxyz"
     document.addEventListener('keydown', (event) => {
         let arrayDiv = [...document.getElementsByClassName("note")]
+        let isLowerCase = false
+
+        for(let i = 0;i<alphabet.length;i++){
+            if (alphabet[i] == event.key){
+                isLowerCase = true
+            }
+        }
 
         if (event.key == "Backspace"){
             arrayDiv[arrayDiv.length-1].remove()
@@ -9,7 +18,7 @@ export function compose(){
             for(let i=0;i<arrayDiv.length;i++){
                 arrayDiv[i].remove()
             }
-        }else{
+        }else if (isLowerCase) {
             let elementDiv = document.createElement("div")
             elementDiv.setAttribute("class","note")
             elementDiv.innerText = event.key
