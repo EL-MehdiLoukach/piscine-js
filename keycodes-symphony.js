851545b798d57a064f71c564ddc4aeb017a72970
pierre <pierre.gourgouillon@ynov.com>
2021-03-24 17:36:26 +0100

    keycodes-symphony

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -5,7 +5,7 @@ export function compose(){
     document.addEventListener('keydown', (event) => {
         let arrayDiv = [...document.getElementsByClassName("note")]
         let isLowerCase = false
-        
+
         for(let i = 0;i<alphabet.length;i++){
             if (alphabet[i] == event.key){
                 isLowerCase = true
@@ -22,7 +22,15 @@ export function compose(){
             let elementDiv = document.createElement("div")
             elementDiv.setAttribute("class","note")
             elementDiv.innerText = event.key
+            let color = randomColor()
+            elementDiv.style.backgroundColor = color
             body.append(elementDiv)
         }
     });
+}
+
+function randomColor() 
+{
+    var r = () => Math.floor(Math.random() * 256);
+    return "rgb(" + r() + "," + r() + "," + r() + ")";
 }
