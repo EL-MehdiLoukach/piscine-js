70831ce8fd3463459a53b57faf90791704f156ed
pierre <pierre.gourgouillon@ynov.com>
2021-03-24 16:00:16 +0100

    fifty-shades-of-cold

--- fifty-shades-of-cold.js
+++ fifty-shades-of-cold.js
@@ -13,7 +13,8 @@ export function generateClasses(){
 export function generateColdShades(){
     let body = document.querySelector('body')
     for(let i =0;i<colors.length;i++){
-        if (colors[i]=="aqua" || colors[i]=="blue"|| colors[i]=="turquoise"|| colors[i]=="green" || colors[i]=="cyan"|| colors[i]=="navy"||colors[i]=="purple"){
+        if (colors[i].includes("aqua") || colors[i].includes("blue")|| colors[i].includes("turquoise")|| colors[i].includes("green") || colors[i].includes("cyan")|| colors[i].includes("navy")||colors[i].includes("purple")){
+            colors[i].con
             let elementDiv = document.createElement("div")
             elementDiv.innerText = colors[i]
             elementDiv.setAttribute("class",colors[i])
