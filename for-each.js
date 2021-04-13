3630a83a42c6d93d5a9f30db3e68f8ae0e67f564
pierre <pierre.gourgouillon@ynov.com>
2021-04-13 17:40:01 +0200

    foreach

--- for-each.js
+++ for-each.js
@@ -1,7 +1,7 @@
 function forEach(array,funct){
     let tab = []
     for (let i=0;i <array.length;i++){
-        tab.push(funct(i))
+        tab.push(funct(array[i],i,tab))
     }
     return tab
 }
