8084aebf547fae39511fe04b934082f4651e522a
pierre <pierre.gourgouillon@ynov.com>
2021-04-13 16:38:42 +0200

    long-words

---
+++ long-words.js
@@ -0,0 +1,27 @@
+function longWords(array1){
+    const t = array1.every((element) =>{
+        return element.length >= 5
+    })
+
+    return t
+}
+
+function oneLongWord(array1){
+    const t = array1.some((element)=>{
+        return element.length >=10
+    })
+    return t
+}
+
+
+function noLongWords(array1){
+    const t = array1.every((element)=>{
+        return element.length >= 7
+    })
+
+    if (!t){
+        return true
+    }else {
+        return false
+    }
+}
