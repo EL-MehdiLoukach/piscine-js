9a67ab16b0387cf70fbbed6e431e29100f9967c8
pierre <pierre.gourgouillon@ynov.com>
2021-04-13 14:46:17 +0200

    using-map

--- using-map.js
+++ using-map.js
@@ -58,10 +58,9 @@ function trimTemp(arrayObjects){
 }
 
 function tempForecasts(arrayObjects){
-    console.log(arrayObjects)
     const a = arrayObjects.map((element)=>{
         let temp = element.temperature
-        let tempString = parseInt(temp.slice(0,temp.indexOf("°") - 1))
+        let tempString = parseInt(temp.slice(0,temp.indexOf("°")))
         let number  = (tempString -32)* 5/9
         let name = element.state[0].toUpperCase()
         let b = false
@@ -90,9 +89,57 @@ function tempForecasts(arrayObjects){
 
 tempForecasts([
     {
-      city: 'Pasadena',
-      temperature: ' 101 °F',
+      city: 'Los Angeles',
+      temperature: '101°F',
       state: 'california',
-      region: 'West',
+      region: 'West'
+    },
+    {
+      city: 'San Francisco',
+      temperature: '84°F',
+      state: 'california',
+      region: 'West'
+    },
+    {
+      city: 'Miami',
+      temperature: '112°F',
+      state: 'Florida',
+      region: 'South'
+    },
+    {
+      city: 'New York City',
+      temperature: '0°F',
+      state: 'new york',
+      region: 'North East'
+    },
+    {
+      city: 'Juneau',
+      temperature: '21°F',
+      state: 'Alaska',
+      region: 'West'
+    },
+    {
+      city: 'Boston',
+      temperature: '45°F',
+      state: 'massachussetts',
+      region: 'North East'
+    },
+    {
+      city: 'Jackson',
+      temperature: '70°F',
+      state: 'mississippi',
+      region: 'South'
+    },
+    {
+      city: 'Utqiagvik',
+      temperature: '-1°F',
+      state: 'Alaska',
+      region: 'West'
+    },
+    {
+      city: 'Albuquerque',
+      temperature: '95°F',
+      state: 'new mexico',
+      region: 'West'
     }
   ])
