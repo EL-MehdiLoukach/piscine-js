5bea74d55007b0aea0ed3699abb81080e24aaed4
pierre <pierre.gourgouillon@ynov.com>
2021-04-14 15:15:56 +0200

    pick

--- pick-omit.js
+++ pick-omit.js
@@ -2,13 +2,21 @@ function pick(obj,keys){
     let obj1 = {}
 
     let keyObject = Object.keys(obj)
-
-    keys.forEach((element)=>{
+    if (typeof keys == "string"){
         keyObject.forEach((elements)=>{
-            if (element == elements){
-                obj1[element] = obj[element]
+            if (keys == elements){
+                obj1[keys] = obj[keys]
             }
         })
-    })
+    }else {
+        keys.forEach((element)=>{
+            keyObject.forEach((elements)=>{
+                if (element == elements){
+                    obj1[element] = obj[element]
+                }
+            })
+        })
+    }
+
     return obj1
 }
