7aed90e882e5c7255c0dea4e48082b860cdcf9d3
pierre <pierre.gourgouillon@ynov.com>
2021-04-14 16:09:16 +0200

    pick

--- pick-omit.js
+++ pick-omit.js
@@ -24,23 +24,22 @@ function pick(obj,keys){
 }
 
 function omit(obj,keys){
-    let obj1 = {}
-    let keyObject = Object.keys(obj)
-
-    if(typeof keys == "string"){
-        keyObject.forEach((elements)=>{
-            if(keys != elements){
-                obj1[keys] = obj[keys]
-            }
-        })
-    }else{
-        keyObject.forEach((element)=>{
-            keys.forEach((elements)=>{
-                if (element != elements){
-                    obj1[element] = obj[element]
-                }
-            })
-        })
-    }
-    return obj1
+    const obj2 = {}
+    let keysObj = Object.keys(obj)
+    keysObj.forEach(key => {
+        if (key != keys) {
+            obj2[key] = obj[key]
+        }
+    })
+    return obj2
 }
+
+let c = ["name","age"]
+
+let fc = {
+    name:"Pierre",
+    age:45,
+    couleur:"bleu"
+}
+
+console.log(omit(fc,c))
