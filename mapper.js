c77bbe0896901484df9f12f14fce755b66403f74
pierre <pierre.gourgouillon@ynov.com>
2021-04-14 12:19:17 +0200

    mapper

--- mapper.js
+++ mapper.js
@@ -5,3 +5,21 @@ function map(array,funct){
     }
     return array2
 }
+
+
+function flatMap(array,funct){
+    let a = []
+    recursive(array)
+    
+    function recursive(element){
+        for (let i =0;i <element.length;i++){
+            if (typeof element[i]== "object"){
+                recursive(element[i])
+            }else {
+                a.push(funct(element[i]))
+            }
+        }
+    }
+
+    return a
+}
