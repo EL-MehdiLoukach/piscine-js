6647c33a919495db46341b14497295015c64d3fd
pierre <pierre.gourgouillon@ynov.com>
2021-04-14 23:02:59 +0200

    manipulate

--- manipulate-values.js
+++ manipulate-values.js
@@ -30,9 +30,9 @@ function mapValues(obj,funct){
 function reduceValues(obj,funct,kc = 0){
     console.log(`wesh ${kc}`)
     let keyObject = Object.keys(obj)
-    let number = 0
+    
     keyObject.forEach((element,index)=>{
-        number = funct(number, obj[element],index,keyObject,initial)
+        number = funct(number, obj[element],index,keyObject,kc)
     })
     console.log(number)
     return number
