0c69094f8b5f881ea3135b9ae22bfe15cd696cfc
pierre <pierre.gourgouillon@ynov.com>
2021-04-14 11:51:55 +0200

    has-city

--- has-city.js
+++ has-city.js
@@ -3,9 +3,9 @@ function hasCity(country, arrayCities){
     return function(city){
         for (let i =0; i < arrayCities.length;i++){
             if (arrayCities[i]== city){
-                return "<city> is a city from <country>"
+                return `${city} is a city from ${country}`
             }
         }
-        return "<city> is not a city from <country>"
+        return `${city} is not a city from ${country}`
     }
 }
