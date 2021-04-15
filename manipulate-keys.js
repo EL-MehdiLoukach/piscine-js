16f992a2150a0be75e31be09cbf35229a89ac79f
pierre <pierre.gourgouillon@ynov.com>
2021-04-15 11:44:46 +0200

    manipulate keys

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -30,7 +30,7 @@ function mapKeys(obj,funct){
 function reduceKeys(obj,funct,kc){
     console.log(`wesh ${kc}`)
     let keyObject = Object.keys(obj)
-    let number = kc
+    let number = ""
     keyObject.forEach((element,index)=>{
         number = funct(number, element,index,keyObject,kc)
     })
