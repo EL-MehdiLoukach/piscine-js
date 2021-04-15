3838d0d833c82f5b2a5d00a9b6949d236da19aa5
pierre <pierre.gourgouillon@ynov.com>
2021-04-15 21:03:15 +0200

    entries

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -7,12 +7,23 @@ function filterEntries(obj,funct){
     valueObject.forEach((element,index)=>{
         
         let bool = funct([element,obj[element]],index,valueObject)
-        console.log (obj[element])
 
         if(bool){
             entries.push([element,obj[element]])         
         }
 
+    })
+    obj1 = Object.fromEntries(entries)
+    return obj1
+}
+
+function mapEntries(obj,funct){
+    let obj1 = {}
+    let entries = []
+    let keyObject = Object.keys(obj)
+
+    keyObject.forEach((key,index)=>{
+        entries.push([funct([key, obj[key]],index)])
     })
     obj1 = Object.fromEntries(entries)
     return obj1
