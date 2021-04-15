665d2f1cf17a45d0237719bdb67375fa16055b5f
pierre <pierre.gourgouillon@ynov.com>
2021-04-15 11:36:26 +0200

    manipulate keys

---
+++ manipulate-keys.js
@@ -0,0 +1,39 @@
+
+function filterKeys(obj,funct){
+    let obj1 = {}
+    let entries = []
+    let valueObject = Object.keys(obj)
+
+    valueObject.forEach((element,index)=>{
+        let bool = funct(element,index)
+
+        if(bool){
+            entries.push([element,obj[element]])         
+        }
+    })
+    obj1 = Object.fromEntries(entries)
+    return obj1
+}
+
+function mapKeys(obj,funct){
+    let obj1 = {}
+    let entries = []
+    let keyObject = Object.keys(obj)
+
+    keyObject.forEach((key,index)=>{
+        entries.push([funct(key,index),obj[key]])
+    })
+    obj1 = Object.fromEntries(entries)
+    return obj1
+}
+
+function reduceKeys(obj,funct,kc = 0){
+    console.log(`wesh ${kc}`)
+    let keyObject = Object.keys(obj)
+    let number = kc
+    keyObject.forEach((element,index)=>{
+        number = funct(number, obj[element],index,keyObject)
+    })
+    console.log(number)
+    return number
+}
