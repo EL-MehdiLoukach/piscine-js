98460683e6235907e85ddf6efebdc738ea8af52f
pierre <pierre.gourgouillon@ynov.com>
2021-04-15 20:54:15 +0200

    entries

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -3,11 +3,10 @@ function filterEntries(obj,funct){
     let obj1 = {}
     let entries = []
     let valueObject = Object.keys(obj)
-    console.log("helloooo", valueObject)
+    
     valueObject.forEach((element,index)=>{
-        let bool = funct(obj[element],index)
-        
-
+        let bool = funct(obj[element],index,valueObject)
+        console.log("helloooo", bool)
         if(bool){
             entries.push([element,obj[element]])         
         }
