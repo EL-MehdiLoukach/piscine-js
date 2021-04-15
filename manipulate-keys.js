749d7148997825b91da74a09570836b3eee16c76
pierre <pierre.gourgouillon@ynov.com>
2021-04-15 11:58:31 +0200

    manipulate keys

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -34,7 +34,7 @@ function reduceKeys(obj,funct,kc){
     keyObject.forEach((element,index)=>{
         number = funct(number, element,index,keyObject,kc)
     })
-    console.log("wesh la street", obj[0])
+    console.log("wesh la street", keyObject[0])
     console.log(`wesh ${number.replace(kc,"")}`)
 
     return number.replace(kc,"")
