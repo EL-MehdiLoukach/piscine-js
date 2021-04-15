57b41731082dfe0399bfbaef371b791007f590ad
pierre <pierre.gourgouillon@ynov.com>
2021-04-15 16:17:47 +0200

    manipulate entries

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -7,6 +7,7 @@ function filterEntries(obj,funct){
     valueObject.forEach((element,index)=>{
         let bool = funct(obj[element],index)
         let boole = funct(element,index)
+
         if(bool && boole){
             entries.push([element,obj[element]])         
         }
@@ -16,24 +17,9 @@ function filterEntries(obj,funct){
 }
 
 function mapEntries(obj,funct){
-    let obj1 = {}
-    let entries = []
-    let keyObject = Object.keys(obj)
 
-    keyObject.forEach((key,index)=>{
-        entries.push([key,funct(obj[key],index)])
-    })
-    obj1 = Object.fromEntries(entries)
-    return obj1
 }
 
 function reduceEntries(obj,funct,kc = 0){
-    console.log(`wesh ${kc}`)
-    let keyObject = Object.keys(obj)
-    let number = kc
-    keyObject.forEach((element,index)=>{
-        number = funct(number, obj[element],index,keyObject)
-    })
-    console.log(number)
-    return number
+
 }
