06abea5becccc04131796348014c13bf873d7f69
pierre <pierre.gourgouillon@ynov.com>
2021-04-22 15:05:55 +0200

    room clean

--- room-is-clean.js
+++ room-is-clean.js
@@ -1,16 +1,15 @@
 
 function isMyRoomClean(check){
+    
     return new Promise(function(resolve,reject){
-        if (check){
-            setTimeout(resolve("My room is clean"),1000)  
-        }else{
-            setTimeout(reject("My room is not clean"),1000)
-        }
+        let a = setInterval((check)=>{
+            if (check){
+                clearInterval(a)
+                resolve("My room is clean")
+            }else{
+                clearInterval(a)
+                reject("My room is not clean")
+            }
+        },1000)  
     })
 }
-
-/*
-isMyRoomClean(true)
-.then(res => console.log(res))
-.catch(res => console.log(res))
-*/
