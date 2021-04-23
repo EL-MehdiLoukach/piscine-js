289080ac536eb0f3824e3d31ab367a8d961638fe
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-23 16:04:23 +0200

    gougle-search

--- gougle-search.js
+++ gougle-search.js
@@ -23,7 +23,7 @@ function queryServers(serverName,q){
 function gougleSearch(query){
 
     const interval = () => {
-        setTimeout(()=>{},80)
+        setTimeout(()=>{},800)
     }
 
     const promiseServer = () => {
