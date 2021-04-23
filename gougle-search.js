af03c417e6afb66ac5ba4dc000fb43205488286e
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-23 11:42:16 +0200

    gougle-search

--- gougle-search.js
+++ gougle-search.js
@@ -1,4 +1,24 @@
 function queryServers(serverName,q){
-    console.log(serverName)
-    console.log(q)
+
+    const url1 = () => {
+        return new Promise((resolve) => {
+            serverName = serverName.split(" ").join("+")
+            q = q.split(" ").join("+")
+
+            let url = `/${serverName}?q=${q}`
+            resolve(url)
+        })
+    }
+
+    const url2 = () => {
+        return new Promise((resolve) => {
+            serverName = serverName.split(" ").join("+")
+            q = q.split(" ").join("+")
+
+            let url = `/${serverName}_backup?q=${q}`
+            resolve(url)
+        })
+    }
+
+    return Promise.race([url1,url2])
 }
