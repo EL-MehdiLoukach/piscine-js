5dfd5214546452be012fb1ba18350c6dd8e15d89
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-23 10:50:05 +0200

    is-winner

--- is-winner.js
+++ is-winner.js
@@ -4,10 +4,25 @@ function isWinner(country){
 
         db.getWinner(country)
             .then((country) =>{
+                if (country.continent  !== "Europe"){
+                    resolve(country + ' is not what we are looking for because of the continent')
+                }
+
                 db.getResults(country.id)
                     .then((countryResult) => {
-                        console.log(countryResult.length)
+
+                        if(countryResult.length === 0){
+                            resolve(country + ' never was a winner')
+                        }else if(countryResult.length < 3){
+                            resolve(country + ' is not what we are looking for because of the number of times it was champion')
+                        }else{
+                            countryResult.forEach((obj) => {
+                                console.log(typeof obj)
+                            })
+                        }
+
                     })
+                    .catch((noWinWC) => {reject(country + ' never was a winner')})
             })
 
     })
