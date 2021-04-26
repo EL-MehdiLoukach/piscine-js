1907d4add750d1099fcd01c323db910b89867c53
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-26 09:50:45 +0200

    date-is

--- date-is.js
+++ date-is.js
@@ -6,3 +6,14 @@ function isValid(date){
     }
     return false
 }
+
+function isAfter(date1,date2){
+    let objectDate1 = new Date(date1)
+    let objectDate2 = new Date(date2)
+
+    if (objectDate1.getFullYear()>objectDate2.getFullYear()){
+        return date1
+    }else {
+        return date2
+    }
+}
