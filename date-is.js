a182a4bf4a4b7fdd9430826254f8249b8f16e8c1
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-26 10:26:38 +0200

    date-is

--- date-is.js
+++ date-is.js
@@ -9,7 +9,6 @@ function isValid(date){
 
 function isAfter(date1,date2){
     if (isNaN(date1) && isNaN(date2)){
-        console.log("hlleo")
         let objectDate1 = new Date(date1)
         let objectDate2 = new Date(date2)
         if (objectDate1.toString() !== "Invalid Date" && objectDate2.toString()!=="Invalid Date"){
@@ -23,5 +22,3 @@ function isAfter(date1,date2){
     }
     return false
 }
-
-console.log(isAfter("2321-12-21T00:00:00.000Z", "2021-04-26T08:24:09.320Z"))
