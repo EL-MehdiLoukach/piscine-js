ee15b1cd8141a643c0f883fac89e45858ff9677b
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-26 10:20:08 +0200

    date-is

--- date-is.js
+++ date-is.js
@@ -10,21 +10,11 @@ function isValid(date){
 function isAfter(date1,date2){
     let objectDate1 = new Date(date1)
     let objectDate2 = new Date(date2)
-    console.log(objectDate1.getMonth(),objectDate2.getMonth())
+
 
     if (!isNaN(date1) && !isNaN(date2)){
         if (objectDate1.toString() !== "Invalid Date" && objectDate2.toString()!=="Invalid Date" && date1.length >= 7 && date2.length >= 7){
-            if(objectDate1.getFullYear()<=2021 && objectDate2.getFullYear()<=2021){
-                if (objectDate1.getFullYear() > objectDate2.getFullYear() ){
-                    return true
-                }else if (objectDate1.getFullYear() === objectDate2.getFullYear() ){
-                    if (objectDate1.getMonth()>objectDate2.getMonth()){
-                        return true
-                    }
-                }else {
-                    return false
-                }
-            }
+            console.log(objectDate1.getMonth(),objectDate2.getMonth())
         }
     }
     return false
