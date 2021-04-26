df15015779f83bf9e1d22f1007de8763ad605b48
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-26 10:37:02 +0200

    date-is

--- date-is.js
+++ date-is.js
@@ -14,13 +14,11 @@ function isAfter(date1,date2){
         let objectDate2 = new Date(date2)
 
         if (objectDate1.toString() !== "Invalid Date" && objectDate2.toString() !== "Invalid Date"){
-            if(objectDate1.getFullYear() <= 2021 && objectDate2.getFullYear() <= 2021){
                 console.log(objectDate2)
                 if (objectDate1.getFullYear() > objectDate2.getFullYear()){
 
                     return true
                 }
-            }
         }
     }
     return false
