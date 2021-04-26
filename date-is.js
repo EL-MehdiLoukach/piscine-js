af4d7f5bf8e4a6d24c20c46c357f04aeca77bc9b
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-26 09:32:09 +0200

    date-is

--- date-is.js
+++ date-is.js
@@ -1,9 +1,7 @@
 
 function isValid(date){
     if (typeof date == "string"){
-        if (date.length === 9){
-            console.log(date)
-        }
+        console.log(date)
     }
     return false
 }
