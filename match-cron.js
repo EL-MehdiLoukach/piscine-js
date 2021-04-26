b6286fb613d251600284883dc6f794b6f8443186
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-26 12:48:03 +0200

    cron

---
+++ match-cron.js
@@ -0,0 +1,12 @@
+
+function matchCron(cron,date){
+    let minutes = date.getMinutes()
+    let hour = date.getHours()
+    let day = date.getDate()
+    let month = date.getMonth()
+    let dayWeek = date.getDay()
+
+    console.log(cron.split(" "))
+
+
+}
