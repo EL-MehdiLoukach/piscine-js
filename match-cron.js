b58ffc65ae870e2e29c61fdfceb2af81b2e52223
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-26 13:01:41 +0200

    cron

--- match-cron.js
+++ match-cron.js
@@ -5,8 +5,49 @@ function matchCron(cron,date){
     let day = date.getDate()
     let month = date.getMonth()
     let dayWeek = date.getDay()
+    let arrayCron = cron.split(" ")
+    let compteur = 0
+    let compt2 = 0
 
-    console.log(cron.split(" "))
+    for (let i = 0; i <= 4;i++){
+        if (!arrayCron[i].includes("*")){
+            compteur++
+        }
+    }
 
+    for (let i =0;i<arrayCron.length;i++){
+        if (!arrayCron[i].includes("*")){
+            if (arrayCron[i] === minutes){
+                compt2++
+                continue
+            }
 
+            if (arrayCron[i] === hour){
+                compt2++
+                continue
+            }
+
+            if (arrayCron[i] === day){
+                compt2++
+                continue
+            }
+
+            if (arrayCron[i] === month){
+                compt2++
+                continue
+            }
+
+            if (arrayCron[i] === dayWeek){
+                compt2++
+                continue
+            }
+        }
+    }
+
+    if (compt2 === compteur){
+        return true
+    }
+
+    return false
 }
+console.log(matchCron('* * * * 1',new Date('2020-06-01 00:00:00')))
