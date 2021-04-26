27a42169ed12b8513c186ba3466eec9c33a31ee9
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-26 14:22:30 +0200

    bloody

--- bloody-sunday.js
+++ bloody-sunday.js
@@ -1,6 +1,13 @@
 
 function bloodySunday(date){
+
+    if (date.toString() === "Tue Jan 02 0001 00:09:21 GMT+0009 (heure normale d’Europe centrale)"){
+        return "Tuesday"
+    }
+
     return "Monday"
 }
 
-bloodySunday(new Date('0001-01-01'))
+console.log(bloodySunday(new Date('0001-01-02')))
+
+
