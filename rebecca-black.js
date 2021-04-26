dc7d3a64bd5b9d2f7f9f47544c1da03759bfc1ea
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-26 11:03:51 +0200

    rebecca

--- rebecca-black.js
+++ rebecca-black.js
@@ -2,7 +2,7 @@
 function isFriday(date){
     console.log(date.getDay())
     if (typeof date =="object"){
-        if (date.getDay() === 4){
+        if (date.getDay() === 5){
             return true
         }
     }
