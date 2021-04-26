5ebecef688a405d66256fe49b37e162f93ca71a4
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-26 10:53:31 +0200

    date-is

--- date-is.js
+++ date-is.js
@@ -21,7 +21,7 @@ function isBefore(date1,date2){
     return false
 }
 
-function isFuture(date1,date2){
-    console.log(date1,date2)
+function isFuture(date1){
+    console.log(date1 - new Date())
 
 }
