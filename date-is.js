5804f32033955a28e8743765ab7ac16d852e085e
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-26 09:47:06 +0200

    date-is

--- date-is.js
+++ date-is.js
@@ -1,10 +1,8 @@
 
 function isValid(date){
-    if (isNaN(date)){
+    if (!isNaN(date)){
         let a =  new Date(date)
         return a.toString() !== "Invalid Date";
     }
     return false
 }
-
-console.log(isValid("2021-04-26T07:39:35.664Z"))
