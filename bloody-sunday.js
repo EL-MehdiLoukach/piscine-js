458144f0318ed501d1ce21cf2fa683daaa268d74
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-29 14:24:50 +0200

    bloody

--- bloody-sunday.js
+++ bloody-sunday.js
@@ -3,5 +3,9 @@ function bloodySunday(date){
     let date001 = new Date("0001-01-01")
     let result = date - date001
 
-    console.log(result)
+    let seconde = (result * 0.001)
+    let calcul = (seconde * 3600) * 24
+
+    console.log(calcul)
+    return "Monday"
 }
