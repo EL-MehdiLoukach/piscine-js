a3febf651d00ffd078de60ab7e509f8aa61cee11
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-29 17:39:56 +0200

    same-amount

--- same-amount.js
+++ same-amount.js
@@ -3,10 +3,7 @@ function sameAmount(str,reg1,reg2){
     let arr1 = str.match(reg1.global) ?? []
     let arr2 = str.match(reg2.global) ?? []
 
-    console.log(arr1,arr2)
-    console.log(str)
-
-    if (arr1.length == arr2.length){
+    if (arr1.length === arr2.length){
         return true
     }
 
