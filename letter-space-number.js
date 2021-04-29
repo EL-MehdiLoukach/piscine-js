eda48304cc9eb2873de8e541c857e2262939d7cc
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-29 15:46:22 +0200

    letter

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,8 +1,8 @@
 
 function letterSpaceNumber(str){
     let array = []
-    let reg = /\w \d /g
+    let reg = /\w \d[\D\W]/g
     return str.match(reg)
 }
 
-letterSpaceNumber("He is 8 or 9 years old, not 10.")
+console.log(letterSpaceNumber("He is 8 or 9 years old, not 10."))
