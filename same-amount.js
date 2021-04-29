20e66b111ed014435c1275a66433d5e968ba034d
Pierre Gourgouillon <Pierre.gourgouillon@hotmail.com>
2021-04-29 17:51:56 +0200

    same-amount

--- same-amount.js
+++ same-amount.js
@@ -1,8 +1,10 @@
 
 function sameAmount(str,reg1,reg2){
     let re1 = new RegExp(reg1,"g")
+    let re2 = new RegExp(reg2,"g")
+
     let arr1 = str.match(re1) ?? []
-    let arr2 = str.match(reg2) ?? []
+    let arr2 = str.match(re2) ?? []
 
     console.log(re1)
 
